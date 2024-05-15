c1a223b6e45028f649541d4dfcb795625ab7917c
lottalaakso <lottaolivia@gmail.com>
2024-05-15 19:23:25 +0300

    App.js config

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'https://github.com/lottalaakso/IMBd.git';
+
+import Sentiment from 'sentiment';
+const sentiment = new Sentiment();
+//comment
 
 function App() {
+
+  const [phrase, setPhrase] = useState('');
+  const [sentimentScore, setSentimentScore] = useState(null);
+
+  useEffect(() => {
+    setSentimentScore(sentiment.analyze(phrase));
+  }, [phrase]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Sentiment Analysis</h2>
+
+        <input value={phrase} onChange={e => setPhrase(e.target.value)}
+          style={{ padding: '20px', fontSize: '20px', width: '70%', borderRadius: '20px', border: 'white' }}
+        />
+
+        {
+          sentimentScore !== null ?
+            <p>Sentiment Score: {sentimentScore.score}</p>
+            : ''
+        }
+
+  {sentimentScore ? (
+    <p>
+      Sentiment: {sentimentScore.score === 0 ? 'Neutral' : sentimentScore.score > 0 ? 'Positive :)' : 'Negative :('}
+    </p>
+  ) : ''}
+
       </header>
     </div>
   );
